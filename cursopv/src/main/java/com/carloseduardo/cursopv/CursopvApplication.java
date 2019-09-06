d9de1247577c01e35a73412b5534d2ab61edcf87
carlosegermano <carlos.germano@dce.ufpb.br>
2019-09-06 17:29:10 -0300

    Início do projeto

---
+++ cursopv/src/main/java/com/carloseduardo/cursopv/CursopvApplication.java
@@ -0,0 +1,13 @@
+package com.carloseduardo.cursopv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursopvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursopvApplication.class, args);
+	}
+
+}
